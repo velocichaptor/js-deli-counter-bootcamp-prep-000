d179188b6972e60e3e09b61858443feae0d45ff7
Jake Chappelle <jake.m.chappelle@gmail.com>
2020-07-31 16:24:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var katzDeli = [];
+//var katzDeli = [];
 
-function takeANumber(katzDeli, name) {
-  katzDeli.push(`${name}`);
-  return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
-}
+//function takeANumber(katzDeli, name) {
+//  katzDeli.push(`${name}`);
+//  return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
+//}
 
 
 //function nowServing(katzDeli) {
