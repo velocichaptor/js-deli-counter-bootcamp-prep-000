bd97e26275b547e3bb4234109c6b855776ab5ca6
Jake Chappelle <jake.m.chappelle@gmail.com>
2020-07-31 16:23:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,28 +6,28 @@ function takeANumber(katzDeli, name) {
 }
 
 
-function nowServing(katzDeli) {
-  let i = 0;
-  while (i < katzDeli.length) {
-    i++;
-  }
-  if (katzDeli.length === 0) {
-    return "There is nobody waiting to be served!";
-  }
-  else
-  return (`Currently serving ${katzDeli.shift()}.`);
-}
+//function nowServing(katzDeli) {
+//  let i = 0;
+//  while (i < katzDeli.length) {
+//    i++;
+//  }
+//  if (katzDeli.length === 0) {
+//    return "There is nobody waiting to be served!";
+//  }
+//  else
+//  return (`Currently serving ${katzDeli.shift()}.`);
+//}
 
-var line = [];
+//var line = [];
 
-function currentLine(katzDeli) {
-  for (let i = 0; i < katzDeli.length; i++) {
-    line.push(` `+[i+1]+`. `  + katzDeli[i])
-  }
-  if (katzDeli.length === 0) {
-    return "The line is currently empty.";
-  } 
-  else
-  return(`The line is currently:` + line);
-}
+//function currentLine(katzDeli) {
+//  for (let i = 0; i < katzDeli.length; i++) {
+//    line.push(` `+[i+1]+`. `  + katzDeli[i])
+//  }
+//  if (katzDeli.length === 0) {
+//    return "The line is currently empty.";
+  //} 
+  //else
+//  return(`The line is currently:` + line);
+//}
 
